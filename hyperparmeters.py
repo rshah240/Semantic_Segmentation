74f668382174f8b0bfbcaeb5c236294bca97907b
Rachit Shah <rshah240@gmail.com>
2020-10-30 01:20:27 +0530

    Added Function to init vgg weights and added train file

---
+++ hyperparmeters.py
@@ -0,0 +1,3 @@
+epochs = 200
+lr = 0.001
+batch_size = 16
---
+++ train.py
@@ -0,0 +1,51 @@
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from utils import Segnet, DroneDataset
+from torch.utils.data import DataLoader
+import hyperparmeters
+
+train_on_gpu = torch.cuda.is_available()
+
+def get_data_loader():
+    dataset = DroneDataset(img_path='./semantic_drone_dataset/original_images', mask_path='./semantic_drone_dataset/label_images_semantic',
+                           mean=[0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225])
+    train_loader = DataLoader(dataset, batch_size=hyperparmeters.batch_size, shuffle = True)
+    return train_loader
+
+def train():
+    model = Segnet()
+    if train_on_gpu:
+        model.cuda()
+    model.init_vgg16_params()
+    optimizer = optim.Adam(model.parameters(), hyperparmeters.lr)
+    criterion = nn.CrossEntropyLoss()
+    train_loader = get_data_loader()
+    epochs = hyperparmeters.epochs
+    losses = []
+    for i in range(epochs):
+        for batch_i,(images,masks) in enumerate(train_loader):
+            if train_on_gpu:
+                images = images.cuda()
+                masks = masks.cuda()
+
+            optimizer.zero_grad()
+            predicted_mask = model(images)
+            loss = criterion(predicted_mask, masks)
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+        if i % 10 == 0:
+            print("Epochs : {}/{} Loss: {.2f}".format(i,epochs, loss.item()))
+if __name__ == "__main__":
+    train()
+
+
+
+
+
+
+
+
+
+
